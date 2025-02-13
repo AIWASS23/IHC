a0d90ed33131c7ba2da2aae7f99ff3dc8f357428
AIWASS23 <marcelodearaujo677@gmail.com>
2025-02-13 14:05:48 -0300

    Initial Commit

---
+++ IHC/IHCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IHCApp.swift
+//  IHC
+//
+//  Created by Marcelo De Araújo on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct IHCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
